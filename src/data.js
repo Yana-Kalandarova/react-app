b5ca25f8f847f345a72e17dba6b055fae072990a
Yana Kalandarava <yana_kalandarava@epam.com>
2019-05-20 11:28:20 +0300

    add data of contact list

---
+++ src/data.js
@@ -0,0 +1,86 @@
+export default [
+  {
+    name: 'Contact 0',
+    phoneNumber: '+37529xxxxx00',
+  },
+  {
+    name: 'Contact 1',
+    phoneNumber: '+37529xxxxx01',
+  },
+  {
+    name: 'Contact 2',
+    phoneNumber: '+37529xxxxx02',
+  },
+  {
+    name: 'Contact 3',
+    phoneNumber: '+37529xxxxx03',
+  },
+  {
+    name: 'Contact 4',
+    phoneNumber: '+37529xxxxx04',
+  },
+  {
+    name: 'Contact 5',
+    phoneNumber: '+37529xxxxx05',
+  },
+  {
+    name: 'Contact 6',
+    phoneNumber: '+37529xxxxx06',
+  },
+  {
+    name: 'Contact 7',
+    phoneNumber: '+37529xxxxx07',
+  },
+  {
+    name: 'Contact 8',
+    phoneNumber: '+37529xxxxx08',
+  },
+  {
+    name: 'Contact 9',
+    phoneNumber: '+37529xxxxx09',
+  },
+  {
+    name: 'Contact 10',
+    phoneNumber: '+37529xxxxx10',
+  },
+  {
+    name: 'Contact 11',
+    phoneNumber: '+37529xxxxx11',
+  },
+  {
+    name: 'Contact 12',
+    phoneNumber: '+37529xxxxx12',
+  },
+  {
+    name: 'Contact 13',
+    phoneNumber: '+37529xxxxx13',
+  },
+  {
+    name: 'Contact 14',
+    phoneNumber: '+37529xxxxx14',
+  },
+  {
+    name: 'Contact 15',
+    phoneNumber: '+37529xxxxx15',
+  },
+  {
+    name: 'Contact 16',
+    phoneNumber: '+37529xxxxx16',
+  },
+  {
+    name: 'Contact 17',
+    phoneNumber: '+37529xxxxx17',
+  },
+  {
+    name: 'Contact 18',
+    phoneNumber: '+37529xxxxx18',
+  },
+  {
+    name: 'Contact 19',
+    phoneNumber: '+37529xxxxx19',
+  },
+  {
+    name: 'Contact 20',
+    phoneNumber: '+37529xxxxx20',
+  },
+];
