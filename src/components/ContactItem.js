4e60c79b91f89542bd619dcd5b7261c87d24d380
Yana Kalandarava <yana_kalandarava@epam.com>
2019-05-21 08:29:30 +0300

    add state to ContactItem component

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import DeleteBtn from './DeleteBtn';
 
 const propTypes = {
   contact: PropTypes.objectOf(PropTypes.shape({
@@ -9,14 +10,29 @@ const propTypes = {
   })).isRequired,
 };
 
-const ContactItem = ({ contact }) => (
-  <li className="contact_item">
-    <dl>
-      <dt>{contact.name}</dt>
-      <dd>{contact.phoneNumber}</dd>
-    </dl>
-  </li>
-);
+class ContactItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDeleted: false,
+    };
+  }
+
+  render() {
+    const { contact } = this.props;
+
+    return (
+      <li className="contact_item">
+        <dl>
+          <dt>{contact.name}</dt>
+          <dd>{contact.phoneNumber}</dd>
+        </dl>
+        <DeleteBtn />
+      </li>
+    );
+  }
+}
 
 ContactItem.propTypes = propTypes;
 
