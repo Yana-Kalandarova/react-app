3deec5d4296aa26be2b609fde2376e1f47b430d1
Yana Kalandarava <yana_kalandarava@epam.com>
2019-06-03 11:34:18 +0300

    update searchValue reducer

--- src/reducers/searchValue.js
+++ src/reducers/searchValue.js
@@ -1,5 +1,10 @@
-const searchValue = () => {
-
+const searchValue = (state = '', action) => {
+  switch (action.type) {
+    case 'SEARCH_CONTACT':
+      return action.searchValue;
+    default:
+      return state;
+  }
 };
 
 export default searchValue;
