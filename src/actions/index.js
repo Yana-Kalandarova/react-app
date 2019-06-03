f5c72ab56085762683a0bbd28d4bac97d9aaac7f
Yana Kalandarava <yana_kalandarava@epam.com>
2019-06-03 11:44:48 +0300

    add DELETE_CONTACT action

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,8 @@ export const searchContact = searchValue => ({
   type: 'SEARCH_CONTACT',
   searchValue,
 });
+
+export const deleteContact = id => ({
+  type: 'DELETE_CONTACT',
+  id,
+});
--- src/reducers/contactList.js
+++ src/reducers/contactList.js
@@ -1,5 +1,18 @@
-const contactList = () => {
+import { findIndexById } from '../utils/index';
 
+const contactList = (list = [], action) => {
+  switch (action.type) {
+    case 'DELETE_CONTACT':
+    {
+      const deletedIndex = findIndexById(list, action.id);
+
+      return list
+        .slice(0, deletedIndex)
+        .concat(list.slice(deletedIndex + 1));
+    }
+    default:
+      return list;
+  }
 };
 
 export default contactList;
