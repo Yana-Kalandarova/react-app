c19692db5f02967c1db6202d58a4bdf83063d1c6
Yana Kalandarava <yana_kalandarava@epam.com>
2019-06-03 13:59:03 +0300

    add ADD_CONTACT and EDIT_CONTACT actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,13 @@ export const deleteContact = id => ({
   type: 'DELETE_CONTACT',
   id,
 });
+
+export const addContact = contact => ({
+  type: 'ADD_CONTACT',
+  contact,
+});
+
+export const editContact = contact => ({
+  type: 'EDIT_CONTACT',
+  contact,
+});
--- src/reducers/contactList.js
+++ src/reducers/contactList.js
@@ -10,6 +10,25 @@ const contactList = (list = [], action) => {
         .slice(0, deletedIndex)
         .concat(list.slice(deletedIndex + 1));
     }
+
+    case 'ADD_CONTACT':
+    {
+      return [
+        ...list,
+        action.contact,
+      ];
+    }
+
+    case 'EDIT_CONTACT':
+    {
+      const editedIndex = findIndexById(list, action.contact.id);
+
+      return list
+        .slice(0, editedIndex)
+        .concat([action.contact])
+        .concat(list.slice(editedIndex + 1));
+    }
+
     default:
       return list;
   }
